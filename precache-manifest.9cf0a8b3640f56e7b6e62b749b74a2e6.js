92be395414434ab357b24397d79c82496365ac7a
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-10-21 22:45:31 -0300

    Watch

---
+++ precache-manifest.9cf0a8b3640f56e7b6e62b749b74a2e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee718f25ce15aba6ae12988f136086f6",
+    "url": "http://watch.hoyosjuan.com/watch/index.html"
+  },
+  {
+    "revision": "581cc64428865d8bfc57",
+    "url": "http://watch.hoyosjuan.com/watch/static/css/main.53e68720.chunk.css"
+  },
+  {
+    "revision": "d3bc7a6a785404962b7a",
+    "url": "http://watch.hoyosjuan.com/watch/static/js/2.972b6720.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://watch.hoyosjuan.com/watch/static/js/2.972b6720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581cc64428865d8bfc57",
+    "url": "http://watch.hoyosjuan.com/watch/static/js/main.602463de.chunk.js"
+  },
+  {
+    "revision": "4e9b3ee4b841de5cdd37",
+    "url": "http://watch.hoyosjuan.com/watch/static/js/runtime-main.724882dd.js"
+  }
+]);
